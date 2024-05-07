b92e408e51eabec21914643a43aa9127716e211d
gregoriussamuelh@gmail.com <gregoriussamuelh@gmail.com>
2024-05-07 10:59:57 +0700

    Experiment 2.1: Original code, and how it run

---
+++ README.md
@@ -0,0 +1,22 @@
+### Reflection Tutorial 10
+
+2.1 Original code of broadcast chat.
+
+Server screenshot:
+
+![alt text](<assets/Screenshot (1098).png>)
+
+Client 1 screenshot:
+
+![alt text](<assets/Screenshot (1099).png>)
+
+Client 2 screenshot:
+
+![alt text](<assets/Screenshot (1100).png>)
+
+Client 3 screenshot:
+
+![alt text](<assets/Screenshot (1101).png>)
+
+Explanation:
+The server here acts as the one to accept incoming connections from multiple clients and the messages being received from the will be broadcasted to all the clients that are connected to the server. The client will then connect to the server, sends those messages and receive the broadcasted messages by the server that was generated because another client was sending a message to the server in order to broadcast to other users of course besides the server itself as can be seen in client screenshot for the client terminal and the respective numbered clients and their terminal screenshots. This way, the clients can communicate indirectly through the server. Especially in this case that follows three clients and a server communication. 
---
+++ src/bin/client.rs
@@ -0,0 +1,41 @@
+use futures_util::stream::StreamExt;
+use futures_util::SinkExt;
+use http::Uri;
+use tokio::io::{AsyncBufReadExt, BufReader};
+use tokio_websockets::{ClientBuilder, Message};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let uri = "ws://127.0.0.1:2000";
+    let (mut ws_stream, _) = ClientBuilder::<'_>::from_uri(Uri::from_static(uri)).connect().await?;
+
+    let stdin = tokio::io::stdin();
+    let mut stdin = BufReader::new(stdin).lines();
+
+    loop {
+        tokio::select! {
+            incoming = ws_stream.next() => {
+                match incoming {
+                    Some(Ok(msg)) => {
+                        if let Some(text) = msg.as_text() {
+                            println!("Server broadcast: {}", text);
+                        }
+                    }
+                    Some(Err(err)) => return Err(err.into()),
+                    None => return Ok(()),
+                }
+            }
+            res = stdin.next_line() => {
+                match res {
+                    Ok(None) => return Ok(()),
+                    Ok(Some(line)) => {
+                        if let Err(err) = ws_stream.send(Message::text(line)).await {
+                            return Err(err.into());
+                        }
+                    }
+                    Err(err) => return Err(err.into()),
+                }
+            }
+        }
+    }
+}
